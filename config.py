2e3235d8e1309cdac1badc0f9b336b3da69bfa0b
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-05 15:54:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-# Credits: @mrismanaziz
-# Copyright (C) 2022 Pyro-ManUserbot
+# Credits: @mrismanaziz & @excute7
+# Copyright (C) 2022 Pyro-HyperUserbot
 #
-# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# This file is a part of < https://github.com/Ling-ex/Hyper/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+# <https://www.github.com/Ling-ex/Hyper/blob/main/LICENSE/>.
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
+# t.me/HyperSupportQ & t.me/storyQi 
 
 from os import getenv
 
@@ -23,15 +24,15 @@ BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "0.2.0@main"
 BRANCH = getenv("BRANCH", "main")
-CHANNEL = getenv("CHANNEL", "Lunatic0de")
+CHANNEL = getenv("CHANNEL", "storyQi")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
-GROUP = getenv("GROUP", "SharingUserbot")
+GROUP = getenv("GROUP", "HyperSupportQ")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
-REPO_URL = getenv("REPO_URL", "https://github.com/mrismanaziz/PyroMan-Userbot")
+REPO_URL = getenv("REPO_URL", "https://github.com/Ling-ex/Hyper")
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
